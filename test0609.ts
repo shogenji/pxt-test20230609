40200dc46ab6a629bad7fcc4af3c80af232a65be
shogenji <ruirui@ruirui.jp>
2023-06-09 10:46:26 +0900

    更新 pxt.json, test0609.ts, _locales/ja/test0609-strings.json

---
+++ test0609.ts
@@ -0,0 +1,29 @@
+enum TropicalFruit {
+    //% block=banana
+    Banana = 0,
+    //% block=pineapple
+    Pinapple = 1,
+    //% block=coconut
+    Coconut = 2
+}
+
+/**
+ * Pick some fruit and peel it.
+ */
+//% weight=70 icon="\uf185" color=#EC7505
+namespace tropic {
+    /**
+     * Pick a fruit
+     */
+    //% blockId=tropic_pick block="pick a %fruit"
+    export function pick(fruit: TropicalFruit): boolean {
+        return true;
+    }
+    /**
+     * Peel the fruit if possible
+     */
+    //% blockId=tropic_peel block="peel a %fruit"
+    export function peel(fruit: TropicalFruit): boolean {
+        return (fruit == TropicalFruit.Banana);
+    }
+}
